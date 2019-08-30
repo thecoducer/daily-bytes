5c4db7f003ac5e57daee80f17898bb8b31828836
thecoducer <mayukh5741@gmail.com>
2019-08-30 13:58:19 +0530

    update

--- diary_app/models.py
+++ diary_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Entry(models.Model):
-    title = models.CharField(max_length=30, default='')
+    title = models.CharField(max_length=50, default='')
     text = models.TextField()
     date_posted = models.DateTimeField(auto_now_add = True)
     # auto_now_add gets the time when a entry is created
