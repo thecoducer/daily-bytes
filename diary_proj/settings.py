6eebba2a94ba4c4a84cb44b653a0401ff1a66ac8
thecoducer <mayukh5741@gmail.com>
2019-09-06 15:28:38 +0530

    update

--- diary_proj/settings.py
+++ diary_proj/settings.py
@@ -176,15 +176,13 @@ SOCIAL_AUTH_SECRET = config('SOCIAL_AUTH_SECRET')
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details',
     'social_core.pipeline.social_auth.social_uid',
-    'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
-    'social.pipeline.mail.mail_validation',
-    'social_core.pipeline.social_auth.associate_by_email', 
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
+    'social_core.pipeline.social_auth.associate_by_email',
 )
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
