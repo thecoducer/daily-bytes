d08e135370a5a9d02b5c2a09de36f6b7c5b810c4
Mayukh Datta <mayukh5741@gmail.com>
2019-09-07 22:25:48 +0530

    update

--- diary_proj/settings.py
+++ diary_proj/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media/'
@@ -192,4 +193,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-django_heroku.settings(locals())
+""" django_heroku.settings(locals()) """
